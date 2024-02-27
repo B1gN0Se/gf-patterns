e851a03e28fdab9f9f89c4cc39c2774b6dbb61e0
Thiago Pereira <86577632+thpereira1@users.noreply.github.com>
2024-02-27 23:13:47 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,6 @@
 apt update
 apt install golang-go -y
 go install github.com/tomnomnom/gf@latest
-git clone https://github.com/thpereira1/gf-patterns.git
-cd gf-patterns
 mkdir -p "$HOME/.gf"
 mv *.json "/$HOME/.gf"
 cd ..
